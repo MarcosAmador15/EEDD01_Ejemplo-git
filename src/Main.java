39f6efcb2d5682b53ddecd2b8da0d88805c6a011
MarcosAmador15 <mamadoli@myuax.com>
2025-02-07 10:38:11 +0100

    Entrada y salida de datos Sentencia de bifurcacion

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        int vi = 1;
+        double vd = 2.3;
+        char vc = 'a';
+        String cadena = "hola";
+
+        //+ Activa como operador de concatenacion
+        System.out.println("vi" + vi +"vd" + vd);
+
+
+        //Leyendo datos del teclado
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un valor entero");
+        vi=sc.nextInt();
+        System.out.println("Valor entero leido: " + vi);
+
+        if(vi % 2 == 0){
+            System.out.println("El numero: " + vi + " es un numero par");
+        }else {
+            System.out.println("El numero: " + vi + " es un numero impar");
+        }
+
         }
     }
-}
