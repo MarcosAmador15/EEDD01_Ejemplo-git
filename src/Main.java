e4e736673a910a6771dbbd73b68a449f515f2be6
MarcosAmador15 <mamadoli@myuax.com>
2025-02-07 11:28:04 +0100

    Bucles

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int vi = 1;
         double vd = 2.3;
         char vc = 'a';
-        String cadena = "hola";
+        String cad = "hola";
 
         //+ Activa como operador de concatenacion
         System.out.println("vi" + vi +"vd" + vd);
@@ -20,11 +20,67 @@ public class Main {
         vi=sc.nextInt();
         System.out.println("Valor entero leido: " + vi);
 
+        //Sentencia de bifurcacion
         if(vi % 2 == 0){
             System.out.println("El numero: " + vi + " es un numero par");
         }else {
             System.out.println("El numero: " + vi + " es un numero impar");
         }
+
+        //Sentencia de bifurcacion anidada
+        if(vi < 50){
+            System.out.println("El numero: " + vi + " es un numero menor que 50");
+        }else if(vi > 50){
+            System.out.println("El numero: " + vi + " es un numero mayor que 50");
+        }else{
+            System.out.println("El numero: " + vi + " es == 50");
+        }
+
+        //Sentencia switch-case
+        System.out.println("Introduzca un color (R/r,V/v,A/a): ");
+        cad=sc.next();
+        vc =cad.charAt(0);
+
+        switch(vc){
+            case 'R':
+            case 'r':
+                System.out.println("Color Rojo");
+                break;
+            case 'V':
+            case 'v':
+                System.out.println("Color Verde");
+                break;
+            case 'A':
+            case 'a':
+                System.out.println("Color Azul");
+                break;
+
+                default:
+                    System.out.println("Color erroneo");
+        }
+
+        //Bucles cuando se conoce a priori el numero de iteraciones
+        int suma1= 0, suma2= 0;
+        for(int i=0; i<10; i++){
+            suma1=suma1+i;
+            suma2+=i;
+        }
+        System.out.println("Suma1: " + suma1 + " Suma2: " + suma2);
+
+        //Bucles while cuando no es conocido a priori el numero de iteraciones
+        //Bucle que solicite numeros al usuario hasta que introduzca un numero par
+
+        int num= 1;
+        while(num % 2 !=0){
+            int numero= 0;
+            System.out.println("Introduzca un numero para ver si es par. Si es impar se lo volvera a pedir ");
+            numero=sc.nextInt();
+            num = numero%2;
+        }
+        System.out.println("El numero ingresado es par");
+
+
+
 
         }
     }
